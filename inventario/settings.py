7b728d3bdaa42d18b7e5fde1765748857ad75aee
elvisdevB <elvis.burgos.dev@gmail.com>
2023-02-11 23:49:01 -0500

    Cambios Finales

--- inventario/settings.py
+++ inventario/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'inventario',
@@ -93,14 +93,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+}
 
 
 # Password validation
